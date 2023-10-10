0aeb6295aef805b5bad16661e04d056c6ed1fe26
student <katsadzeoto@gmail.com>
2023-10-10 18:44:20 +0400

    add test-complete task

---
+++ Script/firstrun.js
@@ -0,0 +1,27 @@
+﻿var steps = require("steps")
+
+function firstTest() { 
+    steps.printHelloWorld();
+    steps.openOrder();
+    steps.addOrderKeyword();
+    steps.closeOrder(true);
+}
+
+function secondTest() {
+    steps.openOrder();
+    let size = 2;
+
+    for (var i = 0; i < size; i++) {
+        steps.addOrder();
+    }
+
+    for (var i = 0; i < size; i++) {
+        steps.checkOrder(i);
+    }
+    
+    for (var i = 0; i < size; i++) {
+        steps.deleteOrder();
+    }
+
+    closeOrder(true);
+}
